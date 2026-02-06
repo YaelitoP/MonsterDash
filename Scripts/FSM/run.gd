bc29a5a1d3c50f23ae5295d1c589e869dc705405
Yael <Yaelppe@gmail.com>
2026-02-06 19:23:24 -0300

    Corrections for FirstMap and tileSets
    Decoretions correct texture origin, parallax now works fine and player moves better acording to world enviroment

--- Scripts/FSM/run.gd
+++ Scripts/FSM/run.gd
@@ -4,6 +4,7 @@ func enter() -> void:
 	player.animNode = "run"
 	
 func physics_update(_delta: float) -> void:
+
 	player.move_and_slide()
 	if Input.is_action_just_pressed("dash"):
 		transitioned.emit("Dash")
@@ -16,8 +17,20 @@ func physics_update(_delta: float) -> void:
 		return
 	else:
 		player.visuals.scale.x = sign(direction)
-		player.velocity.x = direction * 260.0
+		player.velocity.x = direction * player.speed
 		
 	if player.is_on_wall_only():
 		transitioned.emit("wall")
 		return
+	
+
+func handle_slope_physics():
+	var current_speed = player.speed
+	var floor_normal: Vector2
+	if player.is_on_floor():
+		floor_normal = player.get_floor_normal()
+	
+	if sign(player.velocity.x) != sign(floor_normal.x) and floor_normal.x != 0:
+		current_speed *= player.slow_climb_factor
+	
+	return current_speed
--- Scripts/Player/camera.gd
+++ Scripts/Player/camera.gd
@@ -1,8 +1,8 @@
 extends Camera2D
 
 @export_group("Follow Speeds")
-@export var horizontal_speed: float = 12.0
-@export var vertical_speed: float = 6.0 
+@export var horizontal_speed: float = 7.0
+@export var vertical_speed: float = 4.0 
 
 @export_group("State Modifiers")
 @export var dash_follow_mult: float = 0.8 
@@ -25,6 +25,8 @@ func _physics_process(delta: float) -> void:
 			speed_x *= dash_follow_mult
 		"air":
 			speed_y = vertical_speed
+		"wall":
+			speed_x = horizontal_speed / 2
 			
 	global_position.x = lerp(global_position.x, target_pos.x, speed_x * delta)
 	global_position.y = lerp(global_position.y, target_pos.y, speed_y * delta)
